14e28620160461fbaceb85e09b7986fd0a9e04e6
Snsl <oleisme@gmail.com>
2019-04-04 18:11:09 +0300

    wavy url in title

--- front/src/App.js
+++ front/src/App.js
@@ -9,6 +9,25 @@ import ErrorPage from './Views/Error/Error'
 import Navpanel from './components/Navpanel/Navpanel'
 
 
+/*Matthew's wavy url
+http://matthewrayfield.com/articles/animating-urls-with-javascript-and-emojis/#%E2%96%81%E2%96%82%E2%96%84%E2%96%86%E2%96%88%E2%96%88%E2%96%88%E2%96%87%E2%96%85%E2%96%83
+*/
+function loop() {
+        var i, n, s = '';
+
+        for (i = 0; i < 10; i++) {
+            n = Math.floor(Math.sin((Date.now()/200) + (i/2)) * 4) + 4;
+
+            s += String.fromCharCode(0x2581 + n);
+        }
+
+        document.title =`Mlem! ${s}`;
+
+        setTimeout(loop, 10);
+    }
+
+    loop();
+
 class App extends Component {
   render() {
     return (
