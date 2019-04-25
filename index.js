55b1f46a6dc786881d59f2375ea6f9f058362e36
Snsl <oleisme@gmail.com>
2019-04-25 15:44:08 +0300

    conf

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
-const config = require('./secret')[env];
+const config = require("./secret")[env];
 // var https = require('https');
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-
-
-const helmet = require('helmet');
+const helmet = require("helmet");
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(helmet());
-app.use(helmet.hsts({
-  includeSubDomains: false
-}))
-app.use(express.static(path.join(__dirname, 'front/build')));
+app.use(
+  helmet.hsts({
+    includeSubDomains: false
+  })
+);
+app.use(express.static(path.join(__dirname, "front/client")));
 
-
-
-app.get('/', (req, res) =>
-  {
-    res.status(200).send('Ok');
-  }
-)
+// app.get("/", (req, res) => {
+//   res.status(200).send("Ok");
+// });
 
 //routes
-const gitRoute = require('./routes/git');
-app.use('/git', gitRoute);
-const twitterRoute = require('./routes/twitter');
-app.use('/twitter', twitterRoute);
+const gitRoute = require("./routes/git");
+app.use("/git", gitRoute);
+const twitterRoute = require("./routes/twitter");
+app.use("/twitter", twitterRoute);
 
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 const port = config.server.port;
-app.listen(port, () => console.log(`Mleming on port ${port}!`))
+app.listen(port, () => console.log(`Mleming on port ${port}!`));
