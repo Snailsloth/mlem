dd25222384db0466bdf8f3f5e4aa03456acb6719
Snsl <oleisme@gmail.com>
2019-04-30 11:11:25 +0300

    api - github - return last commit

--- routes/git.js
+++ routes/git.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 const env = process.env.NODE_ENV || "development";
-const config = require("../secret")[env];
 
 const axios = require("axios");
 
@@ -15,17 +14,27 @@ let cache = apicache.middleware;
 /*git */
 
 router.get("/", function(req, res, next) {
-  res.status(200).send(`enter git name as param`);
+  res.status(200).send(`enter /:userId/:repo`);
 });
 
-router.get("/:userId", cache("15 minutes"), function(req, res, next) {
+router.get("/:userId/:repo", cache("15 minutes"), function(req, res, next) {
   let user = req.params.userId;
+  let repository = req.params.repo;
 
   axios
-    .get(`https://api.github.com/users/${user}`)
+    .get(`https://api.github.com/repos/${user}/${repository}/events`)
     .then(response => {
-      // handle success
-      res.json(response.data);
+      /* handle success
+      "find() method returns the value of the first element
+      in the array that satisfies the provided testing function"
+
+      We need PushEvent to get our commit message, so:
+      */
+      let pushEvent = response.data.find(event => {
+        return event.type === "PushEvent";
+      });
+      console.log(pushEvent.payload);
+      res.json(pushEvent.payload);
     })
     .catch(error => {
       // handle error
